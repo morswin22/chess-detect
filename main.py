3226de5b9f41cc8ea1ea600f72b388426ca51ed7
Patryk Janiak <xmorswinx@gmail.com>
2024-12-17 12:57:03 +0100

    Abstract away livestreaming and images

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 import cv2
 
-url = "https://192.168.1.34:8080/video"
+class Livestream:
+    def __init__(self, url):
+        self.capture = cv2.VideoCapture(url)
 
-capture = cv2.VideoCapture(url)
+    def release(self):
+        self.capture.release()
+
+    def read(self):
+        return self.capture.read()
+
+class Image:
+    def __init__(self, path):
+        self.image = cv2.imread(path)
+
+    def release(self):
+        pass
+
+    def read(self):
+        return True, self.image
+
+# capture = Livestream("https://192.168.1.34:8080/video")
+capture = Image("frame.png")
 
 while True:
     ret, frame = capture.read()
@@ -14,6 +33,7 @@ while True:
     cv2.imshow("Live Stream", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.imwrite("frame.png", frame)
         break
 
 capture.release()
